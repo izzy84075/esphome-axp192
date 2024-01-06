78d788207f3a527042a0b69bf3bff30ba0d58716
Mauro Riva <lemariva@gmail.com>
2024-01-06 20:48:06 +0100

    add charging binary sensor

--- components/axp192/sensor.py
+++ components/axp192/sensor.py
@@ -1,11 +1,12 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import i2c, sensor
+from esphome.components import i2c, sensor, binary_sensor
 from esphome.const import CONF_ID,\
     CONF_BATTERY_LEVEL, CONF_BRIGHTNESS, UNIT_PERCENT, ICON_BATTERY, CONF_MODEL
 
+CONF_CHARGING = "charging"
 DEPENDENCIES = ['i2c']
-
 axp192_ns = cg.esphome_ns.namespace('axp192')
 AXP192Component = axp192_ns.class_('AXP192Component', cg.PollingComponent, i2c.I2CDevice)
 AXP192Model = axp192_ns.enum("AXP192Model")
@@ -27,6 +28,7 @@ CONFIG_SCHEMA = cv.Schema({
             accuracy_decimals=1,
             icon=ICON_BATTERY,
         ),
+    cv.Optional(CONF_CHARGING): binary_sensor.binary_sensor_schema(),
     cv.Optional(CONF_BRIGHTNESS, default=1.0): cv.percentage,
 }).extend(cv.polling_component_schema('60s')).extend(i2c.i2c_device_schema(0x77))
 
@@ -41,7 +43,10 @@ def to_code(config):
         conf = config[CONF_BATTERY_LEVEL]
         sens = yield sensor.new_sensor(conf)
         cg.add(var.set_batterylevel_sensor(sens))
-
+    if CONF_CHARGING in config:
+        conf = config[CONF_CHARGING]
+        sens = yield binary_sensor.new_binary_sensor(conf)
+        cg.add(var.set_charging_sensor(sens))
     if CONF_BRIGHTNESS in config:
         conf = config[CONF_BRIGHTNESS]
         cg.add(var.set_brightness(conf))
