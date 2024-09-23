863180c574bff8d907ffa6425e687ddf00b31a1b
Izzy <izzy84075@gmail.com>
2024-09-23 08:56:20 -0700

    More

--- components/axp192/axp192.h
+++ components/axp192/axp192.h
@@ -352,6 +352,7 @@ public:
   void dump_config() override;
   float get_setup_priority() const override;
   void update() override;
+  void loop() override;
 
   void power_off();
   void prepare_sleep();
